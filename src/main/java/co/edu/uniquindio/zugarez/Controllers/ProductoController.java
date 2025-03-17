4f162da58618b61727293ad137f5a5654ef0935a
londgav01S <143038512+londgav01S@users.noreply.github.com>
2025-03-16 21:59:02 -0500

    connection with DB

--- src/main/java/co/edu/uniquindio/zugarez/Controllers/ProductoController.java
+++ src/main/java/co/edu/uniquindio/zugarez/Controllers/ProductoController.java
@@ -6,22 +6,54 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/productos")
+@RequestMapping("/api/productos")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ProductoController {
 
     @Autowired
     private ProductoRepository repository;
 
-    @GetMapping
+    @GetMapping("/obtenerProductos")
+    @CrossOrigin(origins = "http://localhost:3000")
     public List<Producto> getAll() {
         return repository.findAll();
     }
 
-    @PostMapping
+    @PostMapping("/crearProducto")
+    @CrossOrigin(origins = "http://localhost:3000")
     public Producto create(@RequestBody Producto producto) {
+        System.out.println("create producto" + producto.getNombre());
         return repository.save(producto);
     }
+    @PutMapping("/{id}")
+    @CrossOrigin(origins = "http://localhost:3000")
+    public Producto updateProduct(@PathVariable String id, @RequestBody Producto updatedProduct) {
+        Optional<Producto> existingProduct = repository.findById(id);
+        if (existingProduct.isPresent()) {
+            Producto producto = existingProduct.get();
+            producto.setNombre(updatedProduct.getNombre());
+            producto.setPrecio(updatedProduct.getPrecio());
+            producto.setCantidad(updatedProduct.getCantidad());
+            producto.setMarca(updatedProduct.getMarca());
+            producto.setFechaVencimiento(updatedProduct.getFechaVencimiento());
+            producto.setEstado(updatedProduct.getEstado());
+            return repository.save(producto);
+        }
+        throw new RuntimeException("Producto no encontrado");
+    }
+    @DeleteMapping("/{id}")
+    @CrossOrigin(origins = "http://localhost:3000")
+    public void deleteProduct(@PathVariable String id) {
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+        } else {
+            throw new RuntimeException("Producto no encontrado");
+        }
+    }
+
+
 }
 
